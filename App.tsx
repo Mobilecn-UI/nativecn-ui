6816383b5f948493c2c6b1119cbfc658f217c8a1
carlos-garciamoran <46545682+carlos-garciamoran@users.noreply.github.com>
2023-09-25 13:31:27 -0700

    feat: enable dark mode support

--- App.tsx
+++ App.tsx
@@ -1,17 +1,25 @@
-import { View } from 'react-native';
+import { View, useColorScheme } from 'react-native';
 
 import CNButton from './components/CNButton';
 import CNBadge from './components/CNBadge';
 
+const lightTheme = 'bg-white';
+const darkTheme = 'bg-black';
+
 export default function App() {
+  const colorScheme = useColorScheme();
+  const containerClasses = colorScheme === 'light' ? lightTheme : darkTheme;
+
   return (
-    <View className="flex-1 items-center justify-center gap-y-4">
+    <View
+      className={`flex-1 items-center justify-center gap-y-4 ${containerClasses}`}
+    >
       <View className="flex flex-row">
         <CNButton label="Button" />
         <CNButton label="Button" variant="ghost" />
         <CNButton label="Button" variant="destructive" />
       </View>
-      <View className="flex flex-row">
+      <View className="flex flex-row space-x-2">
         <CNBadge label="Badge" />
         <CNBadge label="Badge" variant="ghost" />
         <CNBadge label="Badge" variant="destructive" />
