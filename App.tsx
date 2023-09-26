d11993066a6f945cb8bf01d182380f7615abc63c
carlos-garciamoran <46545682+carlos-garciamoran@users.noreply.github.com>
2023-09-25 23:04:45 -0700

    add prettier

--- App.tsx
+++ App.tsx
@@ -1,13 +1,12 @@
-import { Text, View } from "react-native";
-import { styled } from "nativewind";
+import { useState } from 'react';
+import { Text, View } from 'react-native';
+import { styled } from 'nativewind';
 
-import Avatar from "./components/Avatar";
-import Button from "./components/Button";
-import Badge from "./components/Badge";
-import Card from "./components/Card";
-
-import { Tabs, TabsList, TabsTrigger, TabsContent } from "./components/Tabs";
-import { useState } from "react";
+import Avatar from './components/Avatar';
+import Button from './components/Button';
+import Badge from './components/Badge';
+import Card from './components/Card';
+import { Tabs, TabsList, TabsTrigger, TabsContent } from './components/Tabs';
 
 const StyledView = styled(View);
 
@@ -15,24 +14,23 @@ export default function App() {
   const [activeTab, setActiveTab] = useState(0);
 
   const tabs = [
-    { id: 0, title: "Tab 1" },
-    { id: 1, title: "Tab 2" },
+    { id: 0, title: 'Tab 1' },
+    { id: 1, title: 'Tab 2' },
   ];
+
   return (
     <StyledView className="flex-1 py-20 px-10 dark:bg-black">
-      <Text className="mb-2 text-3xl underline dark:text-white">
-        nativecn-ui
-      </Text>
+      <Text className="mb-2 text-3xl underline dark:text-white">nativecn-ui</Text>
 
       <Tabs>
         <TabsList>
-          {tabs.map((tab) => (
+          {tabs.map(tab => (
             <TabsTrigger
               key={tab.id}
-              title={tab.title}
-              setActiveTab={setActiveTab}
               id={tab.id}
+              title={tab.title}
               activeTab={activeTab}
+              setActiveTab={setActiveTab}
             />
           ))}
         </TabsList>
@@ -46,68 +44,70 @@ export default function App() {
         </TabsContent>
       </Tabs>
 
-      <View>
-        <View className="flex gap-2">
-          <Text className="font-semibold text-xl dark:text-white">Avatar</Text>
-          <StyledView className="flex justify-center flex-row space-x-4">
-            <View>
-              <Avatar
-                src="https://pbs.twimg.com/profile_images/1635394124579999745/oJs7VJ3J_400x400.jpg"
-                fallback="CG"
-              />
-            </View>
-            <View>
-              <Avatar
-                src="https://pbs.twimg.com/profile_images/1603610343905058816/PsPEWMOJ_400x400.jpg"
-                fallback="SS"
-              />
-            </View>
-          </StyledView>
-        </View>
-      </View>
-
-      <View className="mt-10">
-        <View className="flex gap-2">
-          <Text className="font-semibold text-xl dark:text-white">Badge</Text>
-          <StyledView className="flex flex-row space-x-2">
-            <View>
-              <Badge label="Badge" />
-            </View>
-            <View>
-              <Badge label="Badge" variant="ghost" />
-            </View>
-            <View>
-              <Badge label="Badge" variant="destructive" />
-            </View>
-          </StyledView>
-        </View>
-
-        <View className="flex gap-2 mt-4">
-          <Text className="font-semibold text-xl dark:text-white">Button</Text>
-          <StyledView className="flex flex-row space-x-2">
-            <View>
-              <Button label="Button" />
-            </View>
-            <View>
-              <Button label="Button" variant="ghost" />
-            </View>
-            <View>
-              <Button label="Button" variant="destructive" />
-            </View>
-          </StyledView>
-        </View>
-
-        <View className="flex gap-2 mt-4">
-          <Text className="font-semibold text-xl dark:text-white">Card</Text>
-          <View>
-            <Card
-              title="Accelerate UI"
-              description="Enter a new development experience"
-              content="Sleek, easy to use components to build your next mobile app faster. Inspired by the legendary shadcn/ui."
-            />
+      <StyledView>
+        <View>
+          <View className="flex gap-2">
+            <Text className="font-semibold text-xl dark:text-white">Avatar</Text>
+            <StyledView className="flex justify-center flex-row space-x-4">
+              <View>
+                <Avatar
+                  src="https://pbs.twimg.com/profile_images/1635394124579999745/oJs7VJ3J_400x400.jpg"
+                  fallback="CG"
+                />
+              </View>
+              <View>
+                <Avatar
+                  src="https://pbs.twimg.com/profile_images/1603610343905058816/PsPEWMOJ_400x400.jpg"
+                  fallback="SS"
+                />
+              </View>
+            </StyledView>
           </View>
         </View>
-      </View>
+
+        <View className="mt-10">
+          <View className="flex gap-2">
+            <Text className="font-semibold text-xl dark:text-white">Badge</Text>
+            <StyledView className="flex flex-row space-x-2">
+              <View>
+                <Badge label="Badge" />
+              </View>
+              <View>
+                <Badge label="Badge" variant="ghost" />
+              </View>
+              <View>
+                <Badge label="Badge" variant="destructive" />
+              </View>
+            </StyledView>
+          </View>
+
+          <View className="flex gap-2 mt-4">
+            <Text className="font-semibold text-xl dark:text-white">Button</Text>
+            <StyledView className="flex flex-row space-x-2">
+              <View>
+                <Button label="Button" />
+              </View>
+              <View>
+                <Button label="Button" variant="ghost" />
+              </View>
+              <View>
+                <Button label="Button" variant="destructive" />
+              </View>
+            </StyledView>
+          </View>
+
+          <View className="flex gap-2 mt-4">
+            <Text className="font-semibold text-xl dark:text-white">Card</Text>
+            <View>
+              <Card
+                title="Accelerate UI"
+                description="Enter a new development experience"
+                content="Sleek, easy to use components to build your next mobile app faster. Inspired by the legendary shadcn/ui."
+              />
+            </View>
+          </View>
+        </View>
+      </StyledView>
     </StyledView>
   );
 }
--- components/Tabs.tsx
+++ components/Tabs.tsx
@@ -1,30 +1,28 @@
-import React, { useState } from "react";
-import { View, Text, TouchableOpacity, useColorScheme } from "react-native";
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
 
-const Tabs = ({ children }: any) => (
-  <View className="flex-1 gap-2">{children}</View>
-);
+const Tabs = ({ children }: any) => <View className="flex-1 gap-2">{children}</View>;
 
 const TabsList = ({ children }: any) => (
-  <View className="m-2 flex-row  items-center justify-center rounded-md  ">
-    {children}
-  </View>
+  <View className="m-2 flex-row  items-center justify-center rounded-md">{children}</View>
 );
 
-const TabsTrigger = ({ title, setActiveTab, id, activeTab }: any) => (
+interface TabsTriggerProps {
+  id: number;
+  title: string;
+  activeTab: number;
+  setActiveTab: (id: number) => void;
+}
+const TabsTrigger = ({ id, title, activeTab, setActiveTab }: TabsTriggerProps) => (
   <TouchableOpacity
     className={`px-14 py-3 ${
-      activeTab === id
-        ? "bg-black dark:bg-white"
-        : "bg-gray-200 dark:bg-gray-700"
+      activeTab === id ? 'bg-black dark:bg-white' : 'bg-gray-200 dark:bg-gray-700'
     } rounded-md m-2 `}
     onPress={() => setActiveTab(id)}
   >
     <Text
       className={`${
-        activeTab === id
-          ? "text-white dark:text-black"
-          : "text-gray-400 dark:text-gray-500"
+        activeTab === id ? 'text-white dark:text-black' : 'text-gray-400 dark:text-gray-500'
       } font-bold`}
     >
       {title}
@@ -33,9 +31,7 @@ const TabsTrigger = ({ title, setActiveTab, id, activeTab }: any) => (
 );
 
 const TabsContent = ({ activeTab, children }: any) => (
-  <View className="flex-1 p-4">
-    {React.Children.toArray(children)[activeTab]}
-  </View>
+  <View className="flex-1 px-2 py-3">{React.Children.toArray(children)[activeTab]}</View>
 );
 
 export { Tabs, TabsList, TabsTrigger, TabsContent };
