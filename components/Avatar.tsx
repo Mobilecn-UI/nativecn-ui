02e288dc686a4f606f2e63f39e9f8ae3fe8d5e58
carlos-garciamoran <46545682+carlos-garciamoran@users.noreply.github.com>
2023-10-02 11:22:29 -0700

    feat: add classes to Avatar

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -9,24 +9,28 @@ interface AvatarProps {
   width?: number;
   height?: number;
   fallback?: string;
+  imageClasses?: string;
+  fallbackViewClasses?: string;
+  fallbackTextClasses?: string;
 }
 export function Avatar({
   src,
   width = 60,
   height = 60,
-  fallback = 'AB',
+  fallback = '',
+  imageClasses = 'rounded-full',
+  fallbackViewClasses = 'border border-black p-4 rounded-full dark:border-white',
+  fallbackTextClasses = 'text-base text-black dark:text-white',
 }: AvatarProps) {
   const [hasImageError, setHasImageError] = useState(false);
 
   return hasImageError ? (
-    <View className="border border-black px-4 py-4 rounded-full dark:border-white">
-      <Text className="text-base text-center font-bold  text-black dark:text-white">
-        {fallback}
-      </Text>
+    <View className={fallbackViewClasses}>
+      <Text className={fallbackTextClasses}>{fallback}</Text>
     </View>
   ) : (
     <Image
-      className="rounded-full"
+      className={imageClasses}
       contentFit="cover"
       source={src}
       style={{ width, height }}
